b7e3003f78cb75ac08c3edafae2d1c93927b22fc
Gregorio <Greg.del801@gmail.com>
2023-07-28 16:17:26 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Your code here
+
+
+class Cat {
+    constructor(name) {
+        this.name = name
+
+    }
+    speak(){
+        console.log( `${this.name} says meow!`)
+    }
+}
+
+class Dog{
+    constructor(name){
+        this.name = name
+    }
+    speak(){
+        return `${this.name} says woof!`
+    }
+}
+
+class Bird{
+    constructor(isMale, name) {
+        this.isMale = isMale
+        this.name = name
+    }
+    speak(){
+        if(this.isMale === true){
+          return  `Its me! ${this.name}`
+        } else{
+            return `${this.name} says squak!`
+        }
+    }
+}
+
+let boyBird = new Bird(true, 'Tim')
+let girlBird = new Bird(false, 'Dolly')
